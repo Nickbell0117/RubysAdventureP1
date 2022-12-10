b4b247643becc729dc84707730ad33a3485758d0
Nickbell0117 <nickbell117@icloud.com>
2022-12-09 23:42:42 -0800

    Fixed bot and added animator controller to ruby
    Fixed the bot problem, and added ruby animator controller

--- RubysAdventureBelloso/Assets/Scripts/RubyController.cs
+++ RubysAdventureBelloso/Assets/Scripts/RubyController.cs
@@ -18,9 +18,14 @@ public class RubyController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
+
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         rigidbody2d = GetComponent<Rigidbody2D>();
 
         currentHealth = maxHealth;
@@ -29,8 +34,20 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+       horizontal = Input.GetAxis("Horizontal");
+       vertical = Input.GetAxis("Vertical");
+
+       Vector2 move = new Vector2(horizontal, vertical);
+
+       if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y,0.0f))
+       {
+              lookDirection.Set(move.x, move.y);
+              lookDirection.Normalize();
+       }
+
+       animator.SetFloat("Look X", lookDirection.x);
+       animator.SetFloat("Look Y", lookDirection.y);
+       animator.SetFloat("Speed", move.magnitude);
     }
     void FixedUpdate()
     {
