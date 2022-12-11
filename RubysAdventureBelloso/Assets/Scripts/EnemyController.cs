051e11e1bb334da5b3781c5d6bf28c7ab3224f05
Nickbell0117 <nickbell117@icloud.com>
2022-12-11 01:40:54 -0800

    Almost done with game
    need to make fixes but added jambi, smoke particles, cog, and UI

--- RubysAdventureBelloso/Assets/Scripts/EnemyController.cs
+++ RubysAdventureBelloso/Assets/Scripts/EnemyController.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float speed = 3.0f;
+    public float speed;
     public bool vertical;
     public float changeTime = 3.0f;
 
+    public ParticleSystem smokeEffect;
+    
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
-
+    bool broken = true;
+    
     Animator animator;
-
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -22,45 +25,67 @@ public class EnemyController : MonoBehaviour
         animator = GetComponent<Animator>();
     }
 
-    void FixedUpdate()
-    {
-        Vector2 position = rigidbody2D.position;
-
-        if(vertical)
-        {
-            position.y = position.y + Time.deltaTime * speed * direction; ;
-            animator.SetFloat("Move X", 0);
-            animator.SetFloat("MOve Y", direction);
-        }
-        else
-        {
-            position.x = position.x + Time.deltaTime * speed * direction; ;
-            animator.SetFloat("Move X", direction);
-            animator.SetFloat("Move Y", 0);
-        }
-
-        rigidbody2D.MovePosition(position);
-    }
-    // Update is called once per frame
     void Update()
     {
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+        if(!broken)
+        {
+            return;
+        }
+        
         timer -= Time.deltaTime;
 
-        if(timer < 0)
+        if (timer < 0)
         {
             direction = -direction;
             timer = changeTime;
         }
     }
-
+    
+    void FixedUpdate()
+    {
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+        if(!broken)
+        {
+            return;
+        }
+        
+        Vector2 position = rigidbody2D.position;
+        
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed * direction;
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
+        }
+        
+        rigidbody2D.MovePosition(position);
+    }
+    
     void OnCollisionEnter2D(Collision2D other)
     {
-        RubyController player = other.gameObject.GetComponent<RubyController>();
-        
+        RubyController player = other.gameObject.GetComponent<RubyController >();
+
         if (player != null)
         {
             player.ChangeHealth(-1);
         }
     }
-
+    
+    //Public because we want to call it from elsewhere like the projectile script
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+        //optional if you added the fixed animation
+        animator.SetTrigger("Fixed");
+        
+        smokeEffect.Stop();
+    }
 }
