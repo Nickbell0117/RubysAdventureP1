3374d8e0b018db7a48ff146c5b12747b52e83b06
Nickbell0117 <nickbell117@icloud.com>
2022-12-11 23:06:29 -0800

    Finally Finished Game
    Still have some issues but I tried my best

--- RubysAdventureBelloso/Assets/Scripts/DamageZone.cs
+++ RubysAdventureBelloso/Assets/Scripts/DamageZone.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class DamageZone : MonoBehaviour
 {
-void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D other)
     {
-        RubyController controller = other.GetComponent<RubyController>();
+        RubyController controller = other.GetComponent<RubyController >();
+
         if (controller != null)
         {
             controller.ChangeHealth(-1);
--- RubysAdventureBelloso/Assets/Scripts/NonPlayerCharacter.cs
+++ RubysAdventureBelloso/Assets/Scripts/NonPlayerCharacter.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NonPlayerCharecter : MonoBehaviour
+public class NonPlayerCharacter : MonoBehaviour
 {
     public float displayTime = 4.0f;
     public GameObject dialogBox;
